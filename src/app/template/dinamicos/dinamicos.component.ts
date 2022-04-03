3cbe4a35e6faf14a2417c78bd1d987e0ea2627bf
Isidro Sotoca <Isidre10_92@hotmail.es>
2022-04-03 16:41:08 +0200

    add template/dinamic page

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[]
+}
+
+interface Favorito {
+  id: number;
+  nombre: string;
+}
 
 @Component({
   selector: 'app-dinamicos',
@@ -6,11 +16,34 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
 
-  constructor() { }
+  nuevoJuego: string = '';
 
-  ngOnInit(): void {
+  persona: Persona = {
+    nombre: 'Isidro',
+    favoritos: [
+      { id: 1, nombre: 'Mario Kart'},
+      { id: 2, nombre: 'Metroid'},
+      { id: 3, nombre: 'Fire Emblem'}
+    ]
+  }
+
+  eliminar ( index: number ): void {
+    this.persona.favoritos.splice(index, 1);
+  }
+
+  agregar(): void {
+    const nuevoFavorito: Favorito = {
+      id: this.persona.favoritos.length + 1,
+      nombre: this.nuevoJuego
+    };
+    this.persona.favoritos.push({...nuevoFavorito});
+    this.nuevoJuego = '';
+  }
+
+  guardar() {
+    console.log("formulario guardado");
   }
 
 }
