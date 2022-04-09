b801208bc2bd42b6731d3284d1107c1c9f485436
Isidro Sotoca <Isidre10_92@hotmail.es>
2022-04-10 00:51:35 +0200

    add pattern validations to form

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -8,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  //todo: temporal
+  nombreApellidoPattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+  emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['', [ Validators.required, Validators.pattern(this.nombreApellidoPattern) ]],
+    email: ['', [ Validators.required,  Validators.pattern(this.emailPattern) ]]
+  });
+
+  constructor( private fb: FormBuilder ) { }
 
   ngOnInit(): void {
+
+    this.miFormulario.reset({
+      nombre: 'Isidro Sotoca',
+      email: 'test1@test.com'
+    });
+  }
+
+  campoNoValido( campo: string ) {
+    return this.miFormulario.get(campo)?.invalid && this.miFormulario.get(campo)?.touched;
+  }
+
+  submitFormulario() {
+
   }
 
 }
